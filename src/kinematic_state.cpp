7c95d0aaadb0ea4caef61087b91f3adecad4507f
sebastian-sampayo <sebisampayo@gmail.com>
2017-09-04 10:29:09 -0300

    Redisign: Position -> Point

--- src/kinematic_state.cpp
+++ src/kinematic_state.cpp
@@ -1,19 +1,6 @@
 #include "kinematic_state.h"
-#include "position.h"
+#include "point.h"
 
 // ----------------------------------------------------------------------------
 // PUBLIC
 // ----------------------------------------------------------------------------
-
-// Assignment operator
-KinematicState& KinematicState::operator=(const KinematicState& other)
-{
-  if (&other != this)
-  {
-    this->position = other.position;
-    this->speed = other.speed;
-    this->accel = other.accel;
-  }
-
-  return *this;
-}
