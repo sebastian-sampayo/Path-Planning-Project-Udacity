37d1b7d1304b15c3b55eb32e910aa9278eb27962
sebastian-sampayo <sebisampayo@gmail.com>
2017-09-11 11:32:20 -0300

    Update stub with Frenet coordinates
    (inconsistent with the Cartesian coordinates already there)

--- src/tests/stub.h
+++ src/tests/stub.h
@@ -11,14 +11,15 @@ json stub_ego_data = {
   {"x", 0},
   {"y", 0},
   {"s", 0},
-  {"d", 0},
+  {"d", 6},
   {"yaw", 0},
-  {"speed", 0}
+  {"speed", 50}
 };
 
+// Be carefull: Cartesian and Frenet coordinates not related!
 json stub_environment_data = json::array({
-  {0, 0, 0, 0, 0, 0, 0},
-  {1, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 50, 6, 0, 0},
+  {1, 1, 1, 100, 2, 0, 0},
   {8, 8, 8, 0, 0, 0, 0},
   {5, 5, 5, 0, 0, 0, 0}
 });
