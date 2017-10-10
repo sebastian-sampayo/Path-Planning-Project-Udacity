d9cbcd222f9e918ce47aa2dff5d6b7d8fca9acd4
sebastian-sampayo <sebisampayo@gmail.com>
2017-10-10 15:33:33 -0300

    Pass objects by reference!

--- src/utils.h
+++ src/utils.h
@@ -17,10 +17,10 @@ double distance(double x1, double y1, double x2, double y2);
 //! Calculates the magnitude of the vector (x,y)
 double Magnitude(double x, double y);
 
-int ClosestWaypoint(double x, double y, vector<double> maps_x, vector<double> maps_y);
+int ClosestWaypoint(double x, double y, const vector<double>& maps_x, const vector<double>& maps_y);
 
 int NextWaypoint(double x, double y, double theta
-  , vector<double> maps_x, vector<double> maps_y);
+  , const vector<double>& maps_x, const vector<double>& maps_y);
   
 template<class Tin, class Tout>
 vector<Tout> CArrayToVector(const Tin* c_array, const int length)
